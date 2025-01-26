4e1460e0b125ed2c33c24fad71dc4c8dc7808e18
albertofullstack <alberto.fullstack@gmail.com>
2025-01-26 20:40:33 +0100

    Add comment.

--- FunctionalInterfaces/src/com/java/eight/features/functionalinterfaces/lambda/GetLenghtTest.java
+++ FunctionalInterfaces/src/com/java/eight/features/functionalinterfaces/lambda/GetLenghtTest.java
@@ -4,6 +4,7 @@ public class GetLenghtTest {
 
   public static void main(String[] args) {
 
+    // the functional interface is the type of the lambda expression
     GetLenghtInterf i = String::length;
     System.out.println(i.getLenght("Hello"));
     System.out.println(i.getLenght("without Lambda"));
