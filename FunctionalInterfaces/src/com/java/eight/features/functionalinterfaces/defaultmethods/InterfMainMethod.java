1987ed87447616f8d9b45c43055084e4d27cf6b7
Alberto Morante <amoranteb@minsait.com>
2025-01-28 21:02:06 +0100

    Add example of main method in an interface.

---
+++ FunctionalInterfaces/src/com/java/eight/features/functionalinterfaces/defaultmethods/InterfMainMethod.java
@@ -0,0 +1,9 @@
+package com.java.eight.features.functionalinterfaces.defaultmethods;
+
+public interface InterfMainMethod {
+
+  public static void main(String[] args) {
+    System.out.println("Main method in interface");
+  }
+
+}
