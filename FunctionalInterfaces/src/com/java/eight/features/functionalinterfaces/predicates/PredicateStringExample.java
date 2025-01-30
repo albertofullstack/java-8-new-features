0098b69d6cfb8be34ab4517e8930f5a9709813c9
Alberto Morante <amoranteb@minsait.com>
2025-01-30 20:24:56 +0100

    Add another implementation for PredicateStringExample.

--- FunctionalInterfaces/src/com/java/eight/features/functionalinterfaces/predicates/PredicateStringExample.java
+++ FunctionalInterfaces/src/com/java/eight/features/functionalinterfaces/predicates/PredicateStringExample.java
@@ -1,18 +1,15 @@
 package com.java.eight.features.functionalinterfaces.predicates;
 
 import java.util.function.Predicate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class PredicateStringExample {
-
-  private static final Logger LOGGER = Logger.getLogger( PredicateStringExample.class.getName() );
 
   public static void main(String[] args) {
     String[] names = {"Kumar", "Juan", "Kiko", "Antonio", "Kara", "Rodrigo", "Paco"};
 //    Predicate<String> startsWithK = i -> i.charAt(0) == 'K'; //another way
     Predicate<String> startsWithK = i -> i.startsWith("K");
-    for (String name: names) {
+    System.out.println("The names starts with K are:");
+    for (String name : names) {
       if (startsWithK.test(name)) {
         System.out.println(name);
       }
