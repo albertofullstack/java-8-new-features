8e6dde5b15e0e5d74e31dbf1f53048e97f167f1c
Alberto Morante <amoranteb@minsait.com>
2025-02-04 20:11:46 +0100

    Add consumer basic example.

---
+++ FunctionalInterfaces/src/com/java/eight/features/functionalinterfaces/consumer/ConsumerExample.java
@@ -0,0 +1,13 @@
+package com.java.eight.features.functionalinterfaces.consumer;
+
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+  public static void main(String[] args) {
+
+    Consumer<String> c = System.out::println;
+    c.accept("hello");
+    c.accept("sir/madame");
+  }
+}
