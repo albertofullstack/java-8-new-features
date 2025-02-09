a7ad68f0ca59664089e139ebc58d91af26d04581
Alberto Morante <amoranteb@minsait.com>
2025-02-09 20:16:04 +0100

    Modify example of method reference.

--- FunctionalInterfaces/src/com/java/eight/features/functionalinterfaces/methodreference/RunnableWithMethodRef.java
+++ FunctionalInterfaces/src/com/java/eight/features/functionalinterfaces/methodreference/RunnableWithMethodRef.java
@@ -2,9 +2,15 @@ package com.java.eight.features.functionalinterfaces.methodreference;
 
 public class RunnableWithMethodRef {
 
+  public void m1() {
+    for (int i = 0; i < 10; i++) {
+      System.out.println("Child thread");
+    }
+  }
+
   public static void main(String[] args) {
 
-    Test t = new Test();
+    RunnableWithMethodRef t = new RunnableWithMethodRef();
     Runnable r = t::m1;
 
     Thread th = new Thread(r);
