4bc97d3d6e149f93d6bcb0511fb8a228c69b7c24
Alberto Morante <amoranteb@minsait.com>
2025-02-09 11:42:33 +0100

    Add examples of primitives supplier.

---
+++ FunctionalInterfaces/src/com/java/eight/features/functionalinterfaces/primitiveinterfaces/PrimitiveSupplierExample.java
@@ -0,0 +1,18 @@
+package com.java.eight.features.functionalinterfaces.primitiveinterfaces;
+
+import java.util.Random;
+import java.util.function.IntSupplier;
+
+public class PrimitiveSupplierExample {
+
+  public static void main(String[] args) {
+    IntSupplier getRandomNumber = () -> new Random().nextInt(10);
+    System.out.println(getRandomNumber.getAsInt());
+
+    String otp = "";
+    for (int i = 0; i < 10; i++) {
+      otp += getRandomNumber.getAsInt();
+    }
+    System.out.println("OTP code: " + otp);
+  }
+}
